edd30e50ca540b34a436ef3a43b1a8f45d3050e4
Mohamed Megahed <megantosh@aim.com>
2018-04-10 19:10:56 +0200

    launchIntent works

--- src/Prototype2/lambda/custom/index.js
+++ src/Prototype2/lambda/custom/index.js
@@ -7,31 +7,56 @@ var Alexa = require("alexa-sdk");
 
 exports.handler = function(event, context) {
     var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(handlers_de_DE, handlers_en_US);
     alexa.execute();
 };
 
-var handlers = {
+var handlers_en_US = {};
+
+var handlers_de_DE = {
     'LaunchRequest': function () {
         this.emit('SayHello');
     },
-    'HelloWorldIntent': function () {
-        this.emit('SayHello');
-    },
+    // 'HelloWorldIntent': function () {
+    //     this.emit('SayHello');
+    // },
     'MyNameIsIntent': function () {
         this.emit('SayHelloName');
     },
+    'openingHoursIntent': function () {
+        this.emit('SayOpeningHours')
+    },
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
     'SayHello': function () {
-        this.response.speak('Hello World!')
-                     .cardRenderer('hello world', 'hello world');
+        this.response.speak('Ich kann dir mit den zahlreichen Dienstleistungen der Stadt Berlin helfen! Was brauchst du genau?')
+        //probably remove. Tacky here and takes away the idea of "voice app"
+                     .cardRenderer('Willkommen beim virtuellen Service-Assistent der Stadt Berlin',
+                         'Disclaimer: Beta-Version - This skill is in no way affiliated with Berlin.de website' +
+                         ' - Alle Angaben ohne Gewähr');
         this.emit(':responseReady');
     },
+    'SayOpeningHours': function () {
+        var district = this.event.request.intent.slots.District.value;
+        this.response.speak('You are in ' + district);
+        this.emit(':responseReady')
+    },
+
+
+
     'SayHelloName': function () {
         var name = this.event.request.intent.slots.name.value;
         this.response.speak('Hello ' + name)
             .cardRenderer('hello world', 'hello ' + name);
         this.emit(':responseReady');
     },
+
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
     'SessionEndedRequest' : function() {
         console.log('Session ended with reason: ' + this.event.request.reason);
     },
@@ -40,8 +65,8 @@ var handlers = {
         this.emit(':responseReady');
     },
     'AMAZON.HelpIntent' : function() {
-        this.response.speak("You can try: 'alexa, hello world' or 'alexa, ask hello world my" +
-            " name is awesome Aaron'");
+        this.response.speak("You can try: 'alexa, until when is Standesamt Friedrichshain-Kreuzberg open today'" +
+            "or 'alexa, ask hello world my name is awesome Aaron'");
         this.emit(':responseReady');
     },
     'AMAZON.CancelIntent' : function() {
@@ -49,7 +74,7 @@ var handlers = {
         this.emit(':responseReady');
     },
     'Unhandled' : function() {
-        this.response.speak("Sorry, I didn't get that. You can try: 'alexa, hello world'" +
-            " or 'alexa, ask hello world my name is awesome Aaron'");
+        this.response.speak("Sorry, I didn't get that. You can try: 'alexa, how do I transfer my driving license to a german one?'" +
+            " or 'alexa, ask Berlin D. E. when is the nearest city hall open today.'");
     }
 };
